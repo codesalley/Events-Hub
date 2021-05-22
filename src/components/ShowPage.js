d06e433c554681916300922142a44a47c58f7780
juxsalley <juxsalley@gmail.com>
2021-05-22 13:23:23 +0000

    show page reponsive

--- src/components/ShowPage.js
+++ src/components/ShowPage.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-underscore-dangle */
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { useState, useEffect } from 'react';
 import SplashScreen from './SplashScreen';
-// import { HeaderDiv, Button } from '../style/ShowPageStyle';
 
 const ShowPage = (props) => {
   const [event, setEvent] = useState();
   const [loading, setLoading] = useState(true);
   const { events } = props;
   const { param } = useParams();
+  const history = useHistory();
 
   useEffect(() => {
     const showEvent = events.filter((ele) => (ele.id === param));
@@ -20,7 +20,7 @@ const ShowPage = (props) => {
   }, []);
 
   return loading || !event ? <SplashScreen /> : (
-    <div className="flex md:px-11 flex-col items-center gap-2">
+    <div className="flex md:px-11 flex-col items-center gap-1 pb-2">
 
       <img className="w-full  " alt="event mg" src={event[0].images[9].url} />
       <div className="flex flex-col gap-2">
@@ -38,7 +38,7 @@ const ShowPage = (props) => {
       </div>
       <div className="flex flex-col gap-1">
         <h2 className="text-center text-xl font-semibold text-gray-700">  Event Location </h2>
-        <p className="text-center text-gray-600 text-sm">
+        <p className="text-center text-gray-600 text-sm flex flex-col">
           Country:
 
           <span>
@@ -47,25 +47,25 @@ const ShowPage = (props) => {
           </span>
           {' '}
         </p>
-        <p className="text-center text-gray-600 text-sm">
+        <p className="text-center text-gray-600 text-sm flex flex-col">
           Venue:
           {' '}
           <span>{event[0]._embedded.venues[0].name}</span>
           {' '}
         </p>
-        <p className="text-center text-gray-600 text-sm">
+        <p className="text-center text-gray-600 text-sm flex flex-col">
           PostalCode:
           {' '}
           <span>{event[0]._embedded.venues[0].postalCode}</span>
           {' '}
         </p>
-        <p className="text-center text-gray-600 text-sm">
+        <p className="text-center text-gray-600 text-sm flex flex-col">
           Address:
           {' '}
           <span>{event[0]._embedded.venues[0].address.line1}</span>
           {' '}
         </p>
-        <p className="text-center text-gray-600 text-sm">
+        <p className="text-center text-gray-600 text-sm flex flex-col">
           Promoter:
           {' '}
           <span>{event[0].promoter.name}</span>
@@ -73,6 +73,7 @@ const ShowPage = (props) => {
         </p>
 
       </div>
+      <button onClick={() => history.push('/')} className="bg-blue-500 text-gray-200 p-2 w-6/12 rounded" type="button"> Go Back </button>
 
     </div>
   );
