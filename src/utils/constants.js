3b9ae9f84ced24490d8fefe3aef5c4c14dc25c24
juxsalley <juxsalley@gmail.com>
2021-05-22 16:15:01 +0000

    update env keys

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const EVENTS_URL = (size) => `https://app.ticketmaster.com/discovery/v2/events.json?size=${size}&apikey=${process.env.REACT_APP_API_KEY}`;
-export const EVENTS_BY_ID_URL = (id) => `https://app.ticketmaster.com/discovery/v2/events/${id}.json?apikey=${process.env.REACT_APP_API_KEY}`;
-export const EVENTS_BY_COUNTRY_CODE = (size, code) => `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=${code}&size=${size}&apikey=${process.env.REACT_APP_API_KEY}`;
+export const EVENTS_URL = (size) => `https://app.ticketmaster.com/discovery/v2/events.json?size=${size}&apikey=qvJS91z3THGXRLihGKEAQ68USVAYffAh`;
+export const EVENTS_BY_ID_URL = (id) => `https://app.ticketmaster.com/discovery/v2/events/${id}.json?apikey=qvJS91z3THGXRLihGKEAQ68USVAYffAh`;
+export const EVENTS_BY_COUNTRY_CODE = (size, code) => `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=${code}&size=${size}&apikey=qvJS91z3THGXRLihGKEAQ68USVAYffAh`;
